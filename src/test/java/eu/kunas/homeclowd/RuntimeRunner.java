4bd113f28d637e7368d26a74d10bf795ed7a9181
Ramazan Kunas <ramazan@kunas.eu>
2015-06-26 14:12:13 +0200

    VideoJS

--- src/test/java/eu/kunas/homeclowd/RuntimeRunner.java
+++ src/test/java/eu/kunas/homeclowd/RuntimeRunner.java
@@ -14,17 +14,21 @@ public class RuntimeRunner {
     @Ignore
     public void runtimeExec() throws Exception {
         String s = new File(".").getAbsolutePath().replace(".","") + "db\\bin\\server.bat";
-        String s2 = "C:\\Users\\kunas\\Dev\\Projekte\\homeclowd\\db\\bin\\server.bat";
+        String s2 = "";
         System.out.println(s2);
 
         Runtime r = Runtime.getRuntime();
+
+        ProcessBuilder p = new ProcessBuilder();
+        p.command("cmd /c C:\\Users\\kunas\\Dev\\Projekte\\homeclowd\\db\\bin\\server.bat");
+        p.start();
 
 
         String[] e = {"ORIENTDB_HOME=C:\\Users\\kunas\\Dev\\Projekte\\homeclowd\\db"};
 
         System.out.println();
 
-        r.exec("cmd /c start "+s2, e);
+       // r.exec("cmd /c start "+s2, e);
 
     }
 }
