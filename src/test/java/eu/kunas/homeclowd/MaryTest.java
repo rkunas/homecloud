b8c8c25129ddce26f71f5c67541b8f2c9537eea0
ramazan <ramazan@kunas.eu>
2015-06-27 19:42:12 +0200

    Removed empty lines

---
+++ src/test/java/eu/kunas/homeclowd/MaryTest.java
@@ -0,0 +1,22 @@
+package eu.kunas.homeclowd;
+
+import org.junit.Test;
+
+import javax.sound.sampled.AudioInputStream;
+
+/**
+ * Created by developer on 27.06.15.
+ */
+public class MaryTest {
+
+    @Test
+    public void testMary(){
+       // MaryInterface marytts = new LocalMaryInterface();
+       // Set<String> voices = marytts.getAvailableVoices();
+       // marytts.setVoice(voices.iterator().next());
+       // AudioInputStream audio = marytts.generateAudio("Hello world.");
+       // AudioPlayer player = new AudioPlayer(audio);
+       // player.start();
+       // player.join();
+    }
+}
