4ad7530340f49939b0e1dba434f370f8704731fe
Ramazan <ramazan@kunas.eu>
2015-07-08 20:25:31 +0200

    3D Viewer complete

--- src/main/java/eu/kunas/homeclowd/frontend/pages/player/StlviewerPage.java
+++ src/main/java/eu/kunas/homeclowd/frontend/pages/player/StlviewerPage.java
@@ -2,7 +2,14 @@ package eu.kunas.homeclowd.frontend.pages.player;
 
 import eu.kunas.homeclowd.common.dto.MediaDto;
 import eu.kunas.homeclowd.frontend.template.TemplatePage;
+import org.apache.wicket.markup.html.basic.Label;
+import org.apache.wicket.markup.html.link.DownloadLink;
+import org.apache.wicket.request.handler.resource.ResourceStreamRequestHandler;
 import org.apache.wicket.request.mapper.parameter.PageParameters;
+import org.apache.wicket.util.resource.FileResourceStream;
+import org.apache.wicket.util.resource.IResourceStream;
+
+import java.io.File;
 
 /**
  * Created by ramazan on 13.04.15.
@@ -14,6 +21,18 @@ public class StlviewerPage extends TemplatePage {
     public StlviewerPage(final PageParameters parameters, MediaDto mediaDto) {
         super("HOMECLOWD - STL Viewer");
         this.selectedMediaDto = mediaDto;
+
+        add(new DownloadLink("downloadButton", new File(selectedMediaDto.getAbsolutePath()), "download") {
+            @Override
+            public void onClick() {
+                File file = (File) getModelObject();
+                IResourceStream resourceStream = new FileResourceStream(new org.apache.wicket.util.file.File(file));
+
+                getRequestCycle().scheduleRequestHandlerAfterCurrent(new ResourceStreamRequestHandler(resourceStream, file.getName()));
+            }
+        });
+
+        add(new Label("filename", "File: " + selectedMediaDto.getDescription()));
     }
 
     public StlviewerPage(){
