0405440c7247bc088e5edf23391a52d6c71e500e
Ramazan <ramazan@kunas.eu>
2015-07-15 23:28:32 +0200

    Version Build Number in Default Page

--- src/main/java/eu/kunas/homeclowd/frontend/pages/auth/SignInPage.java
+++ src/main/java/eu/kunas/homeclowd/frontend/pages/auth/SignInPage.java
@@ -1,18 +1,18 @@
 /**
- *  Licensed to the Apache Software Foundation (ASF) under one or more
- *  contributor license agreements.  See the NOTICE file distributed with
- *  this work for additional information regarding copyright ownership.
- *  The ASF licenses this file to You under the Apache License, Version 2.0
- *  (the "License"); you may not use this file except in compliance with
- *  the License.  You may obtain a copy of the License at
- *
- *       http://www.apache.org/licenses/LICENSE-2.0
- *
- *  Unless required by applicable law or agreed to in writing, software
- *  distributed under the License is distributed on an "AS IS" BASIS,
- *  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
- *  See the License for the specific language governing permissions and
- *  limitations under the License.
+ * Licensed to the Apache Software Foundation (ASF) under one or more
+ * contributor license agreements.  See the NOTICE file distributed with
+ * this work for additional information regarding copyright ownership.
+ * The ASF licenses this file to You under the Apache License, Version 2.0
+ * (the "License"); you may not use this file except in compliance with
+ * the License.  You may obtain a copy of the License at
+ * <p/>
+ * http://www.apache.org/licenses/LICENSE-2.0
+ * <p/>
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
  */
 package eu.kunas.homeclowd.frontend.pages.auth;
 
@@ -44,11 +44,10 @@ import java.util.Properties;
 
 public class SignInPage extends LoginTemplatePage {
 
+    Properties p;
+    FeedbackPanel feedbackPanel;
     private UserDto user = new UserDto();
 
-    Properties p;
-
-    FeedbackPanel feedbackPanel;
     public SignInPage() {
         super("Sign In");
     }
@@ -105,7 +104,7 @@ public class SignInPage extends LoginTemplatePage {
                 if (authResult) {
                     feedbackPanel.setVisible(false);
                     continueToOriginalDestination();
-                }else{
+                } else {
                     feedbackPanel.setVisible(true);
                     getPage().error("Login Failed");
                 }
@@ -139,10 +138,10 @@ public class SignInPage extends LoginTemplatePage {
             p = new Properties();
             p.load(SignInPage.class.getClassLoader().getResourceAsStream("build.properties"));
 
-        }catch (FileNotFoundException exc){
+        } catch (FileNotFoundException exc) {
             exc.printStackTrace();
-        }catch (IOException exc){
-        exc.printStackTrace();
+        } catch (IOException exc) {
+            exc.printStackTrace();
         }
     }
 }
--- src/main/java/eu/kunas/homeclowd/frontend/template/FooterPanel.java
+++ src/main/java/eu/kunas/homeclowd/frontend/template/FooterPanel.java
@@ -1,13 +1,36 @@
 package eu.kunas.homeclowd.frontend.template;
 
+import org.apache.wicket.markup.html.basic.Label;
 import org.apache.wicket.markup.html.panel.Panel;
+import org.apache.wicket.model.Model;
+
+import java.io.FileNotFoundException;
+import java.io.IOException;
+import java.util.Properties;
 
 /**
  * Created by ramazan on 10.04.15.
  */
 public class FooterPanel extends Panel {
 
+    Properties p;
+
     public FooterPanel(String id) {
         super(id);
+        handleProperties();
+        add(new Label("buildNo", new Model<>(p.getProperty("version") + " " + p.getProperty("build.date"))));
+    }
+
+    private void handleProperties() {
+
+        try {
+            p = new Properties();
+            p.load(FooterPanel.class.getClassLoader().getResourceAsStream("build.properties"));
+
+        } catch (FileNotFoundException exc) {
+            exc.printStackTrace();
+        } catch (IOException exc) {
+            exc.printStackTrace();
+        }
     }
 }
