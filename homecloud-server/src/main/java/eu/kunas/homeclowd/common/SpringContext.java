4b93299fe78489c7e8ac612d0723b1dbab340f01
Ramazan <ramazan@kunas.eu>
2015-07-20 18:48:02 +0200

    Eye refactoring

--- homecloud-server/src/main/java/eu/kunas/homeclowd/common/SpringContext.java
+++ homecloud-server/src/main/java/eu/kunas/homeclowd/common/SpringContext.java
@@ -13,8 +13,6 @@ import org.springframework.orm.jpa.JpaTransactionManager;
 import org.springframework.orm.jpa.LocalContainerEntityManagerFactoryBean;
 import org.springframework.orm.jpa.vendor.HibernateJpaVendorAdapter;
 import org.springframework.transaction.annotation.EnableTransactionManagement;
-import org.springframework.web.socket.config.annotation.WebSocketConfigurer;
-import org.springframework.web.socket.config.annotation.WebSocketHandlerRegistry;
 
 import javax.persistence.EntityManagerFactory;
 
