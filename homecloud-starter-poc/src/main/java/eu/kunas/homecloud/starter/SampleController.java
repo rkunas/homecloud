ba153c8bde8b11e7c502808bf82cbf995c4ff2fa
Ramazan Kunas <ramazan@kunas.eu>
2015-07-23 11:22:32 +0200

    Mary TTS 5.1.2

---
+++ homecloud-starter-poc/src/main/java/eu/kunas/homecloud/starter/SampleController.java
@@ -0,0 +1,25 @@
+package eu.kunas.homecloud.starter;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+/**
+ * Created by Kunas on 23.07.2015.
+ */
+@Controller
+@EnableAutoConfiguration
+public class SampleController {
+
+    @RequestMapping("/")
+    @ResponseBody
+    String home() {
+        return "Hello World!";
+    }
+
+    public static void main(String[] args) throws Exception {
+        SpringApplication.run(SampleController.class, args);
+    }
+}
