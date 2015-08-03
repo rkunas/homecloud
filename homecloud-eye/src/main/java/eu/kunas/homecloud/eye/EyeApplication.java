5dabd4d4b8655f8473304a997274d06c77041a0b
Ramazan Kunas <ramazan@kunas.eu>
2015-08-03 13:41:51 +0200

    Mary TTS 5.1.2

--- homecloud-eye/src/main/java/eu/kunas/homecloud/eye/EyeApplication.java
+++ homecloud-eye/src/main/java/eu/kunas/homecloud/eye/EyeApplication.java
@@ -1,13 +1,6 @@
 package eu.kunas.homecloud.eye;
 
-import org.springframework.boot.SpringApplication;
-import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.builder.SpringApplicationBuilder;
-import org.springframework.context.annotation.Bean;
-import org.springframework.scheduling.annotation.Async;
-import org.springframework.stereotype.Controller;
-
-import javax.swing.*;
 
 /**
  * Created by ramazan on 16.07.15.
@@ -16,22 +9,14 @@ import javax.swing.*;
 public class EyeApplication {
 
     public static void main(String[] args) throws Exception {
-        SpringApplicationBuilder builder = new SpringApplicationBuilder(EyeApplication.class);
+        SpringApplicationBuilder builder = new SpringApplicationBuilder(EyeModeWindow.class);
 
         builder.headless(false);
 
         builder.run(args);
     }
 
-    @Bean
-    public MainWindow mainWindow() {
-        return new MainWindow();
-    }
 
-    @Bean
-    public SilentRecorder recorder() {
-        return new SilentRecorder();
-    }
 
 
 }
---
+++ homecloud-eye/src/main/java/eu/kunas/homecloud/eye/EyeModeSilent.java
@@ -0,0 +1,19 @@
+package eu.kunas.homecloud.eye;
+
+import org.springframework.context.annotation.Bean;
+
+/**
+ * Created by Kunas on 03.08.2015.
+ */
+public class EyeModeSilent {
+
+    @Bean
+    public MainWindow mainWindow() {
+        return new MainWindow();
+    }
+
+    @Bean
+    public SilentRecorder recorder() {
+        return new SilentRecorder();
+    }
+}
