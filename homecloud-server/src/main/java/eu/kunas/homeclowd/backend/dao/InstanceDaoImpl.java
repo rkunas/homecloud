cab237c113847249ff686fc4a0f7576741c77d12
Ramazan <ramazan@kunas.eu>
2015-08-07 08:33:01 +0200

    Sort by Folder and then File

---
+++ homecloud-server/src/main/java/eu/kunas/homeclowd/backend/dao/InstanceDaoImpl.java
@@ -0,0 +1,38 @@
+/*
+ * Copyright 2015 ramazan.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package eu.kunas.homeclowd.backend.dao;
+
+import eu.kunas.homeclowd.common.model.entity.HCInstanceEntity;
+import java.util.List;
+import javax.persistence.EntityManager;
+import javax.persistence.PersistenceContext;
+import org.springframework.stereotype.Repository;
+
+/**
+ *
+ * @author ramazan
+ */
+@Repository("userDao")
+public class InstanceDaoImpl {
+
+    @PersistenceContext
+    public EntityManager em;
+
+    public List<HCInstanceEntity> findAll() {
+        return em.createQuery("FROM HCInstanceEntity").getResultList();
+    }
+
+}
---
+++ homecloud-server/src/main/java/eu/kunas/homeclowd/backend/service/InstanceServiceImpl.java
@@ -0,0 +1,35 @@
+/*
+ * Copyright 2015 ramazan.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package eu.kunas.homeclowd.backend.service;
+
+import eu.kunas.homeclowd.backend.dao.ConfigDaoImpl;
+import eu.kunas.homeclowd.backend.dao.InstanceDaoImpl;
+import javax.inject.Inject;
+import javax.inject.Named;
+import org.springframework.stereotype.Service;
+
+/**
+ *
+ * @author ramazan
+ */
+@Service(value = "instanceService")
+public class InstanceServiceImpl {
+
+    @Inject
+    @Named("instanceDao")
+    private InstanceDaoImpl instanceDaoImpl;
+
+}
